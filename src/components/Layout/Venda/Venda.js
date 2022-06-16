15f9f027cffd16bbea3ae26949f3069546813354
Fillip Hudson <Filliphudson222@gmail.com>
2022-06-16 20:11:48 -0300

    Home quase finalizada

--- src/components/Layout/Venda/Venda.js
+++ src/components/Layout/Venda/Venda.js
@@ -1,7 +1,11 @@
+import imgVenda from '../../../imgs/venda.png'
+import styles from './Venda.module.css'
+
 function Venda(){
     return (
-        <div>
-            1
+        <div className={styles.venda}>
+            <img src={imgVenda} alt="venda"/>
+            <button>Fale conosco!</button>
         </div>
 
     )
--- src/components/Sessões/Home/Home.js
+++ src/components/Sessões/Home/Home.js
@@ -1,16 +1,30 @@
+import Venda from '../../Layout/Venda/Venda'
 import styles from './Home.module.css'
 
 function Home(){
     return (
-        <div>
+        <div className={styles.home_container}>
             <h1>Que tal fazer um intercâmbio no Canadá?</h1>
             <p>
-            
+                Conheça as principais atrações turísticas do Canadá através de muitas atividades culturais e educacionais, conheça estudantes, professores e funcionários canadenses, aprenda sobre o sistema educacional do país, inglês intensivo diário, muito lazer e certificados de conclusão. E tudo isso por apenas
             </p>
+            <Venda />
 
-            <ul>
-                <li></li>
-            </ul>
+            <div className={styles.}>
+
+            </div>
+
+            <div>
+                
+            </div>
+
+            <div>
+                
+            </div>
+
+            <div>
+                
+            </div>
         </div>
     )
 }
